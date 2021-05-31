789b3367aa163f230d8316158adf386d4c635eaa
Christian Notthoff <christian.notthoff@anu.edu.au>
2021-05-31 13:56:23 +1000

    Initial files

---
+++ src/base/kookaburraApp.C
@@ -0,0 +1,53 @@
+#include "kookaburraApp.h"
+#include "Moose.h"
+#include "AppFactory.h"
+#include "ModulesApp.h"
+#include "MooseSyntax.h"
+
+InputParameters
+kookaburraApp::validParams()
+{
+  InputParameters params = MooseApp::validParams();
+
+  // Do not use legacy material output, i.e., output properties on INITIAL as well as TIMESTEP_END
+  params.set<bool>("use_legacy_material_output") = false;
+
+  return params;
+}
+
+kookaburraApp::kookaburraApp(InputParameters parameters) : MooseApp(parameters)
+{
+  kookaburraApp::registerAll(_factory, _action_factory, _syntax);
+}
+
+kookaburraApp::~kookaburraApp() {}
+
+void
+kookaburraApp::registerAll(Factory & f, ActionFactory & af, Syntax & syntax)
+{
+  ModulesApp::registerAll(f, af, syntax);
+  Registry::registerObjectsTo(f, {"kookaburraApp"});
+  Registry::registerActionsTo(af, {"kookaburraApp"});
+
+  /* register custom execute flags, action syntax, etc. here */
+}
+
+void
+kookaburraApp::registerApps()
+{
+  registerApp(kookaburraApp);
+}
+
+/***************************************************************************************************
+ *********************** Dynamic Library Entry Points - DO NOT MODIFY ******************************
+ **************************************************************************************************/
+extern "C" void
+kookaburraApp__registerAll(Factory & f, ActionFactory & af, Syntax & s)
+{
+  kookaburraApp::registerAll(f, af, s);
+}
+extern "C" void
+kookaburraApp__registerApps()
+{
+  kookaburraApp::registerApps();
+}
---
+++ test/src/base/kookaburraTestApp.C
@@ -0,0 +1,62 @@
+//* This file is part of the MOOSE framework
+//* https://www.mooseframework.org
+//*
+//* All rights reserved, see COPYRIGHT for full restrictions
+//* https://github.com/idaholab/moose/blob/master/COPYRIGHT
+//*
+//* Licensed under LGPL 2.1, please see LICENSE for details
+//* https://www.gnu.org/licenses/lgpl-2.1.html
+#include "kookaburraTestApp.h"
+#include "kookaburraApp.h"
+#include "Moose.h"
+#include "AppFactory.h"
+#include "MooseSyntax.h"
+#include "ModulesApp.h"
+
+InputParameters
+kookaburraTestApp::validParams()
+{
+  InputParameters params = kookaburraApp::validParams();
+  return params;
+}
+
+kookaburraTestApp::kookaburraTestApp(InputParameters parameters) : MooseApp(parameters)
+{
+  kookaburraTestApp::registerAll(
+      _factory, _action_factory, _syntax, getParam<bool>("allow_test_objects"));
+}
+
+kookaburraTestApp::~kookaburraTestApp() {}
+
+void
+kookaburraTestApp::registerAll(Factory & f, ActionFactory & af, Syntax & s, bool use_test_objs)
+{
+  kookaburraApp::registerAll(f, af, s);
+  if (use_test_objs)
+  {
+    Registry::registerObjectsTo(f, {"kookaburraTestApp"});
+    Registry::registerActionsTo(af, {"kookaburraTestApp"});
+  }
+}
+
+void
+kookaburraTestApp::registerApps()
+{
+  registerApp(kookaburraApp);
+  registerApp(kookaburraTestApp);
+}
+
+/***************************************************************************************************
+ *********************** Dynamic Library Entry Points - DO NOT MODIFY ******************************
+ **************************************************************************************************/
+// External entry point for dynamic application loading
+extern "C" void
+kookaburraTestApp__registerAll(Factory & f, ActionFactory & af, Syntax & s)
+{
+  kookaburraTestApp::registerAll(f, af, s);
+}
+extern "C" void
+kookaburraTestApp__registerApps()
+{
+  kookaburraTestApp::registerApps();
+}
